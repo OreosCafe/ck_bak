1a6083be35efc0696bb67eaeedc2c804619b0e45
night-raise <summerdown@yandex.com>
2021-09-10 22:08:12 +0800

    :recycle: 为读取签到配置增加缓存
    Signed-off-by: night-raise <summerdown@yandex.com>

--- utils.py
+++ utils.py
@@ -4,6 +4,9 @@ import json5 as json
 
 import env_utils
 
+# 缓存全局的环境
+DATA = {}
+
 
 def get_data() -> object:
     """
@@ -11,6 +14,9 @@ def get_data() -> object:
 
     :return: 签到配置文件对象
     """
+    global DATA
+    if DATA: return DATA
+
     if check_config := os.getenv('CHECK_CONFIG'):
         if not os.path.exists(check_config):
             print(f'错误：环境变量 CHECK_CONFIG 指定的配置文件 {check_config} 不存在！')
@@ -22,6 +28,7 @@ def get_data() -> object:
             exit(1)
 
     try:
-        return json.load(open(check_config, "r", encoding="utf-8"))
+        DATA = json.load(open(check_config, "r", encoding="utf-8"))
+        return DATA
     except ValueError:
         print(f'错误：配置文件 {check_config} 格式不对，请在 https://verytoolz.com/json5-validator.html 中检查格式')
