936183220d3b056cdafcc0d4013f7bfb3cf8388e
Leon <LeoninLyon@gmail.com>
2021-09-18 05:46:37 +0800

    :sparkles: GLaDOS

--- utils_tmp.py
+++ utils_tmp.py
@@ -104,3 +104,46 @@ act_list = [
      'text':'每次扣取0积分'
      },
 ]
+
+
+budget_list = [
+    {
+    "level": "Basic",
+    "budget": 0
+    },
+    {
+    "vip": 0,
+    "level": "Free",
+    "budget": 10
+    },
+    {
+    "vip": 10,
+    "level": "Free",
+    "budget": 10
+    },
+    {
+    "vip": 11,
+    "level": "Edu",
+    "budget": 50
+    },
+    {
+    "vip": 21,
+    "level": "Basic",
+    "budget": 200
+    },
+    {
+    "vip": 31,
+    "level": "Pro",
+    "budget": 500
+    },
+    {
+    "vip": 41,
+    "level": "Team",
+    "budget": 2000
+    },
+    {
+    "vip": 51,
+    "level": "Enterprise",
+    "budget": 5000
+    }
+]
--- utils_ver.py
+++ utils_ver.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-__version__ = '20210918-001'
+__version__ = '20210918-002'
 def print_ver():
 	print(f'当前 checkinpanel 版本: {__version__}，请检查是否需要更新\n')
